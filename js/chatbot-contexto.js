05c615dff369a6ea3636fef18d0adaf7d79dd132
Jose Cumaru Neto <105247629+JoseCumaru@users.noreply.github.com>
2026-05-30 04:12:20 -0400

    Atualizar o chatbot-contexto.js
    Atualiza o contexto do chatbot

--- js/chatbot-contexto.js
+++ js/chatbot-contexto.js
@@ -14,6 +14,7 @@ E-mail de Contato: adrianescumaru@gmail.com
 
 Se o cliente perguntar como solicitar orçamentos, desejar iniciar um projeto, fechar um contrato ou pedir para falar com suporte humano, Mande o link oficial de contato via WhatsApp (https://wa.me/5548988536486) e incentive-o a enviar uma mensagem.
 
+#Importante!: Caso o cliente pergunto algo totalmente fora do contexto deste arquivo, responda que voce é apenas um atendente da acumaru e que so responde sobre coisas da empresa
 
 ## 2. Identidade da Empresa
 
